5748682dde55e5f7369d191e75fa92436f578217
jo junho <97390694+whwnsgh0258@users.noreply.github.com>
2025-02-26 18:12:37 +0900

    [Silver V] Title: 영화감독 숌, Time: 268 ms, Memory: 86292 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+    int n = Integer.parseInt(bf.readLine());
+    int count = 0;
+    int num = 665;
+    while (count < n) {
+      num++;
+      if (String.valueOf(num).contains("666")) {
+        count++;
+      }
+    }
+    System.out.println(num);
+  }
+}
