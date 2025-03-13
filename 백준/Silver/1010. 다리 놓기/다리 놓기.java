dcbb413607582d6e8e87cad72e17403254921512
jo junho <97390694+whwnsgh0258@users.noreply.github.com>
2025-03-13 18:20:19 +0900

    [Silver V] Title: 다리 놓기, Time: 124 ms, Memory: 14668 KB -BaekjoonHub

---
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int t = Integer.parseInt(bf.readLine());
+        for (int i = 0; i < t; i++) {
+            StringTokenizer st = new StringTokenizer(bf.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+            bw.write(result(m, n) + "\n");
+        }
+        bw.close();
+    }
+    private static long result(int n, int r) {
+        if (n == r) return 1;
+        long result = 1;
+        for (int i = 0; i < r; i++) {
+            result *= (n - i);
+            result /= (i + 1);
+        }
+        return result;
+    }
+}
