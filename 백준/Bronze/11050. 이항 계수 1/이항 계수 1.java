60c9ee27b2d36686329a64aa879fe8a6d86bbb65
jo junho <97390694+whwnsgh0258@users.noreply.github.com>
2025-03-30 16:08:50 +0900

    [Bronze I] Title: 이항 계수 1, Time: 108 ms, Memory: 14264 KB -BaekjoonHub

---
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        int n = Integer.parseInt(st.nextToken()), k = Integer.parseInt(st.nextToken());
+        bw.write(result(n, k) + "");
+        bw.close();
+    }
+    private static int result(int n, int k) {
+        if (k > n - k) k = n - k;
+        int result = 1;
+        for (int i = 0; i < k; i++) {
+            result *= (n - i);
+            result /= (i + 1);
+        }
+        return result;
+    }
+}
