585a85cb04fe1ab47f3b02d4be31983cdd74687c
jo junho <97390694+whwnsgh0258@users.noreply.github.com>
2025-06-03 19:52:44 +0900

    [Silver V] Title: 색종이, Time: 104 ms, Memory: 14192 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        boolean[][] paper = new boolean[100][100];
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            for (int row = x; row < x + 10; row++) {
+                for (int col = y; col < y + 10; col++) {
+                    paper[row][col] = true;
+                }
+            }
+        }
+
+        int count = 0;
+        for (int i = 0; i < 100; i++) {
+            for (int j = 0; j < 100; j++) {
+                if (paper[i][j]) count++;
+            }
+        }
+        bw.write(count + "");
+        bw.close();
+    }
+}
